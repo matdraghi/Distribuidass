39587ac167a9cc05bd876226b61387f2408fa043
matdraghi <51202903+matdraghi@users.noreply.github.com>
2019-09-18 18:02:34 -0300

    SINGIN-UP With Users table

--- eclipse-workspace/ReclamosWeb/src/main/java/com/mycompany/myapp/HomeController.java
+++ eclipse-workspace/ReclamosWeb/src/main/java/com/mycompany/myapp/HomeController.java
@@ -21,10 +21,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import exceptions.DocumentException;
 import exceptions.EdificioException;
 import exceptions.InquilinoException;
+import exceptions.LoginException;
 import exceptions.NombreException;
 import exceptions.PersonaException;
 import exceptions.ReclamoException;
 import exceptions.UnidadException;
+import exceptions.UsuarioException;
 import controlador.Controlador;
 import dto.ReclamosViewDTO;
 /**
@@ -78,5 +80,26 @@ public class HomeController {
 		return new ResponseEntity<String>(mapper.toJson(result), HttpStatus.OK);
 	}
 	
+	
+	@ResponseBody
+	@RequestMapping(value = "/Registro", method = RequestMethod.GET)
+	public ResponseEntity<String> Registro(@RequestParam(value = "Documento", required = true) String documento,
+			@RequestParam(value = "Password", required = true) String password)
+			throws DocumentException, NombreException, PersonaException, InquilinoException {
+		boolean result = false;
+		result = Controlador.getInstancia().RegistrarUsuario(documento, password);;
+		JsonMapper mapper = new JsonMapper();
+		return new ResponseEntity<String>(mapper.toJson(result), HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/Login", method = RequestMethod.GET)
+	public ResponseEntity<String> Login(@RequestParam(value = "documento", required = true) String documento,
+			@RequestParam(value = "password", required = true) String password)
+			throws DocumentException, NombreException, PersonaException, InquilinoException, LoginException, UsuarioException, ReclamoException {
+		boolean result = Controlador.getInstancia().login(documento, password);
+		JsonMapper mapper = new JsonMapper();
+		return new ResponseEntity<String>(mapper.toJson(result), HttpStatus.OK);
+	}
 	
 }
