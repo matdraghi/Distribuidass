b29be0bc2a457cc4675fccebb5a83c681092ae1c
matdraghi <51202903+matdraghi@users.noreply.github.com>
2019-09-19 11:55:56 -0300

    Add files via upload

--- eclipse-workspace/ReclamosWeb/src/main/java/com/mycompany/myapp/HelloController.java
+++ eclipse-workspace/ReclamosWeb/src/main/java/com/mycompany/myapp/HelloController.java
@@ -15,7 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;  
 import org.springframework.web.bind.annotation.RequestParam;  
 import org.springframework.web.multipart.commons.CommonsMultipartFile;  
-import org.springframework.web.servlet.ModelAndView;  
+import org.springframework.web.servlet.ModelAndView;
+
+import controlador.Controlador;
+import daos.ReclamosDAO;
+import exceptions.PersonaException;
+import exceptions.ReclamoException;  
   
 @Controller  
 public class HelloController {  
@@ -48,10 +53,31 @@ public class HelloController {
     
     @RequestMapping(value="/savefile",method=RequestMethod.POST)  
     public ModelAndView upload(@RequestParam CommonsMultipartFile file,HttpSession session){  
+    	//@RequestParam(value = "IdReclamo", required = true) int IdReclamo
             String path=session.getServletContext().getRealPath("/");  
             String filename=file.getOriginalFilename();  
+            String filetype = file.getContentType();
+            System.out.println("PATH: " +path);
+            System.out.println("TIPO: " +filetype); // Me devuelve que tipo de imagen tengo
+            /*
+            try {
+				ReclamosDAO.getInstancia().findByIDReclamo(IdReclamo);
+			} catch (ReclamoException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (PersonaException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}*/
+            
+            try {
+				Controlador.getInstancia().CargarImagen(path.toString(), filetype.toString());
+			} catch (ReclamoException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
              // viene aca
-            System.out.println(path+" "+filename);  
+            System.out.println("Direccion en la Pc" + path+" "+filename);  
             try{  
             byte barr[]=file.getBytes();  
               
