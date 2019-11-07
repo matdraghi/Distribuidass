680a658572efbb5661cfed6ce772fdfc3d47649b
matdraghi <51202903+matdraghi@users.noreply.github.com>
2019-11-07 15:12:18 -0300

    Delete DuenioException.java

--- eclipse-workspace/ReclamosAdmin/src/exceptions/DuenioException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class DuenioException extends Exception{
-	private static final long serialVersionUID = 9018648492209155948L;
-
-	public DuenioException(String mensaje) {
-		super(mensaje);
-	}
-}
