4d3dea4cb3647464047d3aabef2b627e1401106e
matdraghi <51202903+matdraghi@users.noreply.github.com>
2019-11-07 15:13:22 -0300

    Delete InquilinoException.java

--- eclipse-workspace/ReclamosAdmin/src/exceptions/InquilinoException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class InquilinoException extends Exception{
-	private static final long serialVersionUID = 955948L;
-
-	public InquilinoException(String mensaje) {
-		super(mensaje);
-	}
-}
