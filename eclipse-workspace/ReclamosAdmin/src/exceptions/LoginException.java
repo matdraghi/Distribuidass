94d125a9acf339b12b70a6e3c79aa1f328c5f099
matdraghi <51202903+matdraghi@users.noreply.github.com>
2019-11-07 15:12:34 -0300

    Delete LoginException.java

--- eclipse-workspace/ReclamosAdmin/src/exceptions/LoginException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class LoginException extends Exception{
-	private static final long serialVersionUID = 9018648448L;
-
-	public LoginException(String mensaje) {
-		super(mensaje);
-	}
-}
